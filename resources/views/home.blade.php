b3188f9ce5c8f15eca4eda1724489c3648cebda6
Luther Karper <luther@Luthers-Air.cable.rcn.com>
2020-11-17 09:25:39 -0500

    Update home view

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,33 @@
-@extends('layouts/app')
+@extends('layouts.app')
 
-@section ('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Dashboard</div>
-                    <div class="card-body">
-                        
-                    </div>
-                </div>
-            </div>
+@section('content')
+<div class="container">
+   <div class="row">
+       <div class="col-9 pt-5">
+           <div class="d-inline-flex justify-content-between">
+               {{-- <h1>{{ $user->username }}</h1> --}}
+               <a href="#">Add new post</a>
+           </div>
+           <div class="d-flex">
+               <div class="pr-5"><strong>153</strong> posts</div>
+               <div class="pr-5"><strong>23k</strong> followers</div>
+               <div class="pr-5"><strong>212</strong> following</div>
+           </div>
+           {{-- <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+           <div>{{ $user->profile->description }}</div>
+           <div><a href="#">{{ $user->profile->url }}</a></div> --}}
+       </div>
+   </div>
+    <div class="row pt-4">
+        <div class="col-4">
+            <img class="w-100 "src="https://images.unsplash.com/photo-1580953870233-4c7f485628d8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80" alt="">
+        </div>
+        <div class="col-4">
+            <img class="w-100 "src="https://images.unsplash.com/photo-1580953870233-4c7f485628d8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80" alt="">
+        </div>
+        <div class="col-4">
+            <img class="w-100 "src="https://images.unsplash.com/photo-1580953870233-4c7f485628d8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80" alt="">
         </div>
     </div>
+</div>
 @endsection
