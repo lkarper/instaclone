6914530d259b4d697394089bb0289a6ebfe6a422
Luther Karper <luther@Luthers-Air.cable.rcn.com>
2020-11-18 09:25:50 -0500

    Uncomment code

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,8 +4,8 @@
 <div class="container">
    <div class="row">
        <div class="col-9 pt-5">
-           <div class="d-inline-flex justify-content-between">
-               {{-- <h1>{{ $user->username }}</h1> --}}
+           <div class="d-inline-flex justify-content-between align-items-baseline">
+               <h1>{{ $user->username }}</h1>
                <a href="#">Add new post</a>
            </div>
            <div class="d-flex">
@@ -13,20 +13,20 @@
                <div class="pr-5"><strong>23k</strong> followers</div>
                <div class="pr-5"><strong>212</strong> following</div>
            </div>
-           {{-- <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
+           <div class="pt-4 font-weight-bold">{{ $user->profile->title }}</div>
            <div>{{ $user->profile->description }}</div>
-           <div><a href="#">{{ $user->profile->url }}</a></div> --}}
+           <div><a href="#">{{ $user->profile->url }}</a></div>
        </div>
    </div>
     <div class="row pt-4">
         <div class="col-4">
-            <img class="w-100 "src="https://images.unsplash.com/photo-1580953870233-4c7f485628d8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80" alt="">
+            <img class="w-100" src="https://images.unsplash.com/photo-1580953870233-4c7f485628d8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80" alt="Lorem ipsum">
         </div>
         <div class="col-4">
-            <img class="w-100 "src="https://images.unsplash.com/photo-1580953870233-4c7f485628d8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80" alt="">
+            <img class="w-100" src="https://images.unsplash.com/photo-1580953870233-4c7f485628d8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80" alt="">
         </div>
         <div class="col-4">
-            <img class="w-100 "src="https://images.unsplash.com/photo-1580953870233-4c7f485628d8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80" alt="">
+            <img class="w-100" src="https://images.unsplash.com/photo-1580953870233-4c7f485628d8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80" alt="">
         </div>
     </div>
 </div>
