eb28b689c3c5264b963e022e8bfe217559db51ab
Luther Karper <luther@Luthers-Air.cable.rcn.com>
2020-11-18 09:26:06 -0500

    Update layout

--- resources/views/profiles/index.blade.php
+++ resources/views/profiles/index.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
    <div class="row">
        <div class="col-9 pt-5">
-           <div class="d-inline-flex justify-content-between">
+           <div class="d-inline-flex justify-content-between align-items-baseline">
                <h1>{{ $user->username }}</h1>
                <a href="#">Add new post</a>
            </div>
@@ -18,9 +18,15 @@
            <div><a href="#">{{ $user->profile->url }}</a></div>
        </div>
    </div>
-    <div class="row pt-4">
+    <div class="row pt-5">
         <div class="col-4">
-
+            <img class="w-100" src="https://images.unsplash.com/photo-1580953870233-4c7f485628d8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80" alt="Lorem ipsum">
+        </div>
+        <div class="col-4">
+            <img class="w-100" src="https://images.unsplash.com/photo-1580953870233-4c7f485628d8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80" alt="">
+        </div>
+        <div class="col-4">
+            <img class="w-100" src="https://images.unsplash.com/photo-1580953870233-4c7f485628d8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80" alt="">
         </div>
     </div>
 </div>
