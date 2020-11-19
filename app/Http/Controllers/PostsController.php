3afec30be4d78e59914855d2590579749a4e9317
Luther Karper <luther@Luthers-Air.cable.rcn.com>
2020-11-19 09:34:33 -0500

    Update PostsController to persist posts in table

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
+use App\Models\Post;
+use Illuminate\Support\Facades\Auth;
 
 class PostsController extends Controller
 {
 
+    // All routes in this controller require auth
     public function __construct()
     {
         $this->middleware('auth');
@@ -18,19 +21,23 @@ class PostsController extends Controller
     }
 
     public function store() {
+        
         $data = request()->validate([
             'caption' => 'required',
             'image' => ['required', 'image'],
         ]);
-        $data['title'] = '';
-        $imagePath = request('image')->store('uploads', 'public');
-        dd($imagePath);
-        auth()->user()->posts()->create([
+
+        $imagePath = request('image')->store('uploads', 'public'); // store(directory, driver)
+        // Run php artisan storage:link to create link to storage/app/public/uploads
+        
+        // Gets the authenticated user to create a post with their id
+        Post::create([
             'caption' => $data['caption'],
             'image' => $imagePath,
+            'user_id' => Auth::id(),
             'title' => '',
         ]);
 
-        return redirect('/profile/' . auth()->user()->id);
+        return redirect('/profile/' . request()->user()->id);
     }
 }
