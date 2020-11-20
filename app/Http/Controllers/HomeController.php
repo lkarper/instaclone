411b1803b296b1e8c86bf8ed5fe8c040cafa542f
Luther Karper <luther@Luthers-Air.cable.rcn.com>
2020-11-20 09:28:58 -0500

    Update HomeController redirect to profile on login

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -24,6 +24,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home', ['user' => Auth::user()]);
+        return view('profiles/index', ['user' => Auth::user()]);
     }
 }
